0de97671452ab1ce6503ebfcba0ecd3e8f1b49f3
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-10-12 12:42:06 +0100

    Add attack functionality

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@
 ENV['RACK_ENV'] = 'test'
 
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
-require 'web_helpers.rb'
+require_relative 'features/web_helpers'
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
