4b6440536b140af868cf33816d7f33a7385dc80e
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-10-14 16:43:00 +0100

    Style changes

--- app.rb
+++ app.rb
@@ -12,21 +12,21 @@ class Battle < Sinatra::Base
 
   post '/names' do
     $game = Game.new(params[:Player1], params[:Player2])
-    redirect '/start_game'
+    redirect '/start-game'
   end
 
-  get '/start_game' do
+  get '/start-game' do
     @game = $game
     erb :start_game
   end
 
-  get '/player_1_turn' do
+  get '/player-1-turn' do
     @game = $game
     @game.attack(@game.player2)
     erb :player_1_turn
   end
 
-  get '/player_2_turn' do
+  get '/player-2-turn' do
     @game = $game
     @game.attack(@game.player1)
     erb :player_2_turn
