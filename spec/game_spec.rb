130e928068b7caad17837f92c8997c005a4997d9
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-10-14 16:26:37 +0100

    Separate the player class from the controller

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,31 +1,32 @@
 require 'game'
 
 describe Game do
-  subject(:game) { described_class.new(player1.name, player2.name) }
-  let (:player1) { double :player1 }
-  let (:player2) { double :player2 }
-
-  before :each do
-    allow(player1).to receive(:name).and_return('John')
-    allow(player2).to receive(:name).and_return('Mary')
-  end
+  subject(:game) { described_class.new('John', 'Mary') }
 
   describe '#player1' do
     it 'retrieves the first player' do
-      expect(game.player1).to eq player1
+      expect(game.player1.name).to eq 'John'
+    end
+
+    it 'is an instance of Player' do
+      expect(game.player1).to be_an_instance_of(Player)
     end
   end
 
   describe '#player2' do
     it 'retrieves the second player' do
-      expect(game.player2).to eq player2
+      expect(game.player2.name).to eq 'Mary'
+    end
+
+    it 'is an instance of Player' do
+      expect(game.player2).to be_an_instance_of(Player)
     end
   end
 
   describe "#attack" do
     it "can attack the opponent" do
-      expect(player2).to receive(:reduce_hp)
-      game.attack(player2)
+      expect(game.player2).to receive(:reduce_hp)
+      game.attack(game.player2)
     end
   end
 end
